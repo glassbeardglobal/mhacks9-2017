097541312fb10f7ca6b39d383a8112ef840a01f3
Salil999 <ssaxen4@illinois.edu>
2017-03-25 04:43:44 -0400

    added 3 wrapper methods for capital one API

--- apis/capital_one.js
+++ apis/capital_one.js
@@ -1,19 +1,101 @@
 'use strict';
 
 const API_KEY = '65be1ee16b2fbd6faade515551491370';
+const BASE_URL = 'http://api.reimaginebanking.com';
+const CARD_TYPE = ['Credit Card', 'Savings', 'Checking'];
 var request = require('request');
 
-function getAllAccountInfo() {
+exports.getAllAccountInfo = function(cb) {
+/* NOTE: RETURNS ARRAY OF OBJECTS
+{
+    _id: "56c66be6a73e492741507b69",
+    rewards: 29461,
+    balance: 38759,
+    customer_id: "56c66be5a73e49274150728d",
+    nickname: "Mrs.'s Account",
+    type: "Checking"
+}
+*/
+
     request({
-        url: 'http://api.reimaginebanking.com/accounts?key=' + API_KEY,
+        url: BASE_URL + '/accounts?key=' + API_KEY,
         method: 'GET'
     }, function(err, res, body) {
-        
+        cb(JSON.parse(err), JSON.parse(body));
     });
 }
 
-function createAccount(id) {
+exports.getAllCustomers = function(cb) {
+/* NOTE: RETURNS ARRAY OF OBJECTS
+{
+    _id: "56c66be5a73e49274150728d",
+    last_name: "McConaughey",
+    address: {
+        city: "Homeworth",
+        street_name: "Bowman Street Northeast",
+        zip: "44634",
+        state: "Ohio",
+        street_number: "24500"
+    },
+    first_name: "Matthew"
+}
+*/
+
     request({
-        url: ''
+        url: BASE_URL + '/customers?key=' + API_KEY,
+        method: 'GET'
+    }, function(err, res, body) {
+        cb(JSON.parse(err), JSON.parse(body));
+    })
+}
+
+exports.createAccount = function(first_name, last_name, cb) {
+/*
+{
+    code: 201,
+    message: "Account created",
+    objectCreated: {
+        type: "Credit Card",
+        nickname: "fstname's account",
+        rewards: 0,
+        balance: 0,
+        customer_id: "58d6283d1756fc834d9064e5",
+        _id: "58d6283d1756fc834d9064e6"
+    }
+}
+*/
+    // First we need to create the customer
+    request({
+        url: BASE_URL + '/customers?key=' + API_KEY,
+        json: {
+            "first_name": first_name,
+            "last_name": last_name,
+            "address": {
+                "street_number": " ",
+                "street_name": " ",
+                "city": " ",
+                "state": "ZZ",
+                "zip": "99999"
+            }
+        },
+        method: 'POST'
+    }, function(err, res, body) {
+        // Once the customer is created, now we create their account
+        // cb(err, body);
+        var id = body.objectCreated._id;
+        var nick = body.objectCreated.first_name;
+        request({
+            url: BASE_URL + '/customers/' + id + '/accounts?key=' + API_KEY,
+            json: {
+                "type": CARD_TYPE[0],
+                "nickname": nick + "'s account",
+                "rewards": 0,
+                "balance": 0,
+            },
+            method: 'POST'
+        }, function(err_n, res_n, body_n) {
+            cb(err_n, body_n);
+        });
     });
 }
+
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,32 @@
 
 var express = require('express');
 var router = express.Router();
+var cap_one = require('../apis/capital_one.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ "success": true });
+	// cap_one.createAccount('fstname', 'lstname', function(err, data) {
+	// 	if(!err) {
+	// 		res.json(data);
+	// 	} else {
+	// 		console.log(err);
+	// 	}
+	// });
+	// cap_one.getAllAccountInfo(function(err, data) {
+	// 	if(!err) {
+	// 		res.json(data);
+	// 	} else {
+	// 		res.status(404).send();
+	// 	}
+	// });
+	// cap_one.getAllCustomers(function(err, data) {
+	// 	if(!err) {
+	// 		res.json(data);
+	// 	} else {
+	// 		res.status(404).send();
+	// 	}
+	// });
+
 });
 
 module.exports = router;
