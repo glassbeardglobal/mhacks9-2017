8c7a1a4e54dc1af716607b401d45a7bbe96824ba
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-03-25 10:09:07 -0400

    Added alphadvantage api calls

--- apis/market.js
+++ apis/market.js
@@ -1,6 +1,9 @@
 var request = require('request');
 
 var base = 'http://dev.markitondemand.com/Api/v2/';
+var aa_base = 'http://www.alphavantage.co/';
+
+var aa_key = '7103';
 
 function lookup(queryString, cb) {
   request({
@@ -26,7 +29,68 @@ function quote(ticker, cb) {
   });
 }
 
+function interdayChart(ticker, numDays, cb) {
+  var dataInput = {
+    "NumberOfDays": numDays,
+    "DataPeriod": "Day",
+    "Elements":[{ "Symbol": ticker, "Type": "price", "Params": ["c"] }]
+  };
+
+  var param = JSON.stringify(dataInput);
+
+  request({
+    uri: '/InteractiveChart/json',
+    baseUrl: base,
+    method: 'GET',
+    qs: { parameters: param },
+    json: true
+  }, function(err, resp, data) {
+    cb(err, data);
+  });
+}
+
+/*
+ * Gets most recent stock data divided into 15 min intervals
+*/
+function intradayChart(ticker, cb) {
+  request({
+    uri: '/query',
+    baseUrl: aa_base,
+    method: 'GET',
+    qs: {
+      function: 'TIME_SERIES_INTRADAY',
+      symbol: ticker,
+      interval: '15min',
+      outputsize: 'full',
+      apikey: aa_key
+    },
+    json: true
+  }, function(err, resp, data) {
+    cb(err, data);
+  });
+}
+
+function dailyChart(ticker, cb) {
+  request({
+    uri: '/query',
+    baseUrl: aa_base,
+    method: 'GET',
+    qs: {
+      function: 'TIME_SERIES_DAILY',
+      symbol: ticker,
+      outputsize: 'full',
+      apikey: aa_key
+    },
+    json: true
+  }, function(err, resp, data) {
+    cb(err, data);
+  });
+}
+
 module.exports = {
   lookup: lookup,
-  quote: quote
+  quote: quote,
+  interdayChart: interdayChart,
+  intradayChart: intradayChart,
+  dailyChart: dailyChart
 }
--- test/apiTest.js
+++ test/apiTest.js
@@ -28,4 +28,24 @@ describe('Market API Functions', function() {
       done();
     });
   });
+
+  it('should get interdayChart data for a ticker', function(done) {
+    this.timeout(5000);
+    market.interdayChart('NVDA', 10, function(err, data) {
+      console.log(data);
+
+      data.should.be.a('object');
+      done();
+    });
+  });
+
+  it('should get intradayChart data for a ticker', function(done) {
+    this.timeout(5000);
+    market.intradayChart('NVDA', function(err, data) {
+      data.should.be.a('object');
+      data.should.have.property('Time Series (15min)');
+
+      done();
+    });
+  });
 });
